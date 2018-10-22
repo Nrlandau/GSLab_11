2cf21a958a88f9f5d41abe8aabfa47debbb0c662
Nicholas Landau <nrlandau@gmail.com>
2018-10-22 10:21:50 -0400

    added stuff

--- Program.cs
+++ Program.cs
@@ -3,19 +3,20 @@ using System.Collections;
 
 namespace Lab11
 {
+    enum MovieCategory{ANIMATED,DRAMA,HORROR,SCIFI};
     class Movie
     {
         //Vars
         private string name;
-        private string category;
+        private MovieCategory category;
         //Constructor
-        public Movie(string _name, string _category)
+        public Movie(string _name, MovieCategory _category)
         {
             name = _name;
             category = _category;
         }
         //Methods
-        public static void GetCagegories(ArrayList _movies, string _category )
+        public static void DisplayMoviesFromCategory(ArrayList _movies, MovieCategory _category )
         {
             foreach(Movie m in _movies)
             {
@@ -23,14 +24,28 @@ namespace Lab11
                     System.Console.WriteLine(m.name);
             }
         }
-
+        public static MovieCategory GetCagegories(int _index)
+        {
+            return (MovieCategory)(_index);
+        }
+        private static MovieCategory SetCategory(string _movieCat)
+        {
+            switch(_movieCat.ToLower())
+            {
+                case "drama": return MovieCategory.DRAMA;
+                case "animated": return MovieCategory.ANIMATED;
+                case "horror": return MovieCategory.HORROR;
+                case "scifi": return MovieCategory.SCIFI;
+                default: throw new ArgumentException("Invalid Category");
+            }
+        }
         //Properties
     }
     class Program
     {
         static void Main(string[] args)
         {
-
+            System.Console.WriteLine(Movie.GetCagegories(4));
         }
     }
 }
